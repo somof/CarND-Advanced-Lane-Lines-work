44510f162014e0ebcc0afd1a3d15bb6e5ee0baf8
somof <gituser@somof.net>
2017-08-02 08:35:40 +0900

    impl. 1st sliding window method

--- src/project_code02-2ndMethod.py
+++ src/project_code02-2ndMethod.py
@@ -397,114 +397,38 @@ def process_image(image, weight=0.5):
     # return cv2.cvtColor(binary_warped, cv2.COLOR_GRAY2BGR)  # debug code
     # return cv2.cvtColor(cv2.warpPerspective(undist, M, (undist.shape[1], undist.shape[0]), flags=cv2.INTER_LINEAR), cv2.COLOR_RGB2BGR)  # debug code
 
-    # 5) Find Lanes via Sliding Windows: 1st Method
+    # 5) Find Lanes via Sliding Windows
+    window_centroids = find_window_centroids(binary_warped, window_width, window_height, margin)
+    if len(window_centroids) > 0:
+        # Points used to draw all the left and right windows
+        l_points = np.zeros_like(binary_warped)
+        r_points = np.zeros_like(binary_warped)
 
-    # Take a histogram of the bottom half of the image
-    histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
-    # Create an output image to draw on and  visualize the result
-    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
-    # Find the peak of the left and right halves of the histogram
-    # These will be the starting point for the left and right lines
-    midpoint = np.int(histogram.shape[0]/2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+        # Go through each level and draw the windows
+        for level in range(0, len(window_centroids)):
+            # Window_mask is a function to draw window areas
+            l_mask = window_mask(window_width, window_height, binary_warped, window_centroids[level][0], level)
+            r_mask = window_mask(window_width, window_height, binary_warped, window_centroids[level][1], level)
+            # Add graphic points from window mask here to total pixels found
+            l_points[(l_points == 255) | ((l_mask == 1))] = 255
+            r_points[(r_points == 255) | ((r_mask == 1))] = 255
 
-    # Choose the number of sliding windows
-    nwindows = 9
-    # Set height of windows
-    window_height = np.int(binary_warped.shape[0]/nwindows)
-    # Identify the x and y positions of all nonzero pixels in the image
-    nonzero = binary_warped.nonzero()
-    nonzeroy = np.array(nonzero[0])
-    nonzerox = np.array(nonzero[1])
-    # Current positions to be updated for each window
-    leftx_current = leftx_base
-    rightx_current = rightx_base
-    # Set the width of the windows +/- margin
-    margin = 100
-    # Set minimum number of pixels found to recenter window
-    minpix = 50
-    # Create empty lists to receive left and right lane pixel indices
-    left_lane_inds = []
-    right_lane_inds = []
+        # Draw the results: make left and right window pixels red and blue
+        templateL = np.array(l_points, np.uint8)
+        templateR = np.array(r_points, np.uint8)
+        zero_channel = np.zeros_like(templateL)
+        template = np.array(cv2.merge((templateR, zero_channel, templateL)), np.uint8)
+        warpage = np.array(cv2.merge((binary_warped, binary_warped, binary_warped)), np.uint8)  # making the original road pixels 3 color channels
+        output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0)  # overlay the orignal road image with window results
 
-    # Step through the windows one by one
-    for window in range(nwindows):
-        # Identify window boundaries in x and y (and right and left)
-        win_y_low = binary_warped.shape[0] - (window+1)*window_height
-        win_y_high = binary_warped.shape[0] - window*window_height
-        win_xleft_low = leftx_current - margin
-        win_xleft_high = leftx_current + margin
-        win_xright_low = rightx_current - margin
-        win_xright_high = rightx_current + margin
-        # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
-        # Identify the nonzero pixels in x and y within the window
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-        # Append these indices to the lists
-        left_lane_inds.append(good_left_inds)
-        right_lane_inds.append(good_right_inds)
-        # If you found > minpix pixels, recenter next window on their mean position
-        if len(good_left_inds) > minpix:
-            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-        if len(good_right_inds) > minpix:        
-            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
-    
-    # Concatenate the arrays of indices
-    left_lane_inds = np.concatenate(left_lane_inds)
-    right_lane_inds = np.concatenate(right_lane_inds)
-    
-    # Extract left and right line pixel positions
-    leftx = nonzerox[left_lane_inds]
-    lefty = nonzeroy[left_lane_inds] 
-    rightx = nonzerox[right_lane_inds]
-    righty = nonzeroy[right_lane_inds] 
-    
-    # Fit a second order polynomial to each
-    left_fit = np.polyfit(lefty, leftx, 2)
-    right_fit = np.polyfit(righty, rightx, 2)
+    else:
+        # If no window centers found, just display orginal road image
+        output = np.array(cv2.merge((binary_warped, binary_warped, binary_warped)), np.uint8)
 
-    # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-    
-    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-    # return out_img
 
-    # # 5) Find Lanes via Sliding Windows: 2nd method
-    # window_centroids = find_window_centroids(binary_warped, window_width, window_height, margin)
-    # if len(window_centroids) > 0:
-    #     # Points used to draw all the left and right windows
-    #     l_points = np.zeros_like(binary_warped)
-    #     r_points = np.zeros_like(binary_warped)
-    # 
-    #     # Go through each level and draw the windows
-    #     for level in range(0, len(window_centroids)):
-    #         # Window_mask is a function to draw window areas
-    #         l_mask = window_mask(window_width, window_height, binary_warped, window_centroids[level][0], level)
-    #         r_mask = window_mask(window_width, window_height, binary_warped, window_centroids[level][1], level)
-    #         # Add graphic points from window mask here to total pixels found
-    #         l_points[(l_points == 255) | ((l_mask == 1))] = 255
-    #         r_points[(r_points == 255) | ((r_mask == 1))] = 255
-    # 
-    #     # Draw the results: make left and right window pixels red and blue
-    #     templateL = np.array(l_points, np.uint8)
-    #     templateR = np.array(r_points, np.uint8)
-    #     zero_channel = np.zeros_like(templateL)
-    #     template = np.array(cv2.merge((templateR, zero_channel, templateL)), np.uint8)
-    #     warpage = np.array(cv2.merge((binary_warped, binary_warped, binary_warped)), np.uint8)  # making the original road pixels 3 color channels
-    #     output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0)  # overlay the orignal road image with window results
-    # 
-    # else:
-    #     # If no window centers found, just display orginal road image
-    #     output = np.array(cv2.merge((binary_warped, binary_warped, binary_warped)), np.uint8)
-    # 
-    # return output
+
+    return output
 
 
 
@@ -517,11 +441,6 @@ def process_image(image, weight=0.5):
 
     # detect offset of the car position
 
-
-
-
-
-    return out_img
 
 
     # Tracking -> tips
